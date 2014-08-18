234c778f9e45d5be42b370094e097c7e8195bd95
Artem Medeusheyev <artem.medeusheyev@gmail.com>
2014-08-18 22:09:19 +0600

    pacakge updates and new helm settings

--- lisp/helm-inits.el
+++ lisp/helm-inits.el
@@ -7,6 +7,9 @@
 (require 'helm-files)
 (require 'helm-grep)
 
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c o") 'helm-occur-from-isearch)
+
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
